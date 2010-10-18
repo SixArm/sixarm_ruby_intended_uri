1df0f0516046f71131bf55abaf97cb9322ff5abc
SixArm <sixarm@sixarm.com>
2010-10-17 21:11:01 -0700

    first

---
+++ lib/sixarm_ruby_intended_uri.rb
@@ -0,0 +1,46 @@
+=begin rdoc
+
+= SixArm Ruby Gem: IntendedUri
+
+Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
+Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
+License:: CreativeCommons License, Non-commercial Share Alike
+License:: LGPL, GNU Lesser General Public License
+
+Get and set the user's intended uri in the Rails session array.
+
+==Example
+  require 'intended_uri'
+  class ApplicationController
+    ...
+    include IntendedUri
+    ...
+    def sign_in_success
+      redirect_to intended_uri || welcome_uri
+    end
+    ...
+    def sign_in_failure
+      intended_uri=request.request_uri
+      redirect_to sign_in
+    end
+
+=end
+
+
+require 'uri'
+
+class IntendedUri
+
+  # Get the user's intended uri in the Rails session array
+
+  def intended_uri
+    session[:intended_uri]
+  end
+
+  # Set the user's intended uri in the Rails session array
+
+  def intended_uri=(uri=request.request_uri)
+    session[:intended_uri] = uri
+  end
+
+end
---
+++ test/sixarm_ruby_intended_uri_test.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+require 'intended_uri'
+require 'session_mock'
+require 'uri'
+
+class IntendedUriTest < Test::Unit::TestCase
+
+ include IntendedUri
+
+ def session
+  @session||=Hash.new
+ end
+
+ def test_nil
+  assert_not_nil(session,"session")
+  assert_nil(intended_uri,"intended_uri")
+ end
+
+ def test_uri
+  assert_not_nil(session,"session")
+  uri=Uri.parse('http://example.com')
+  intended_uri=uri
+  assert_equal(uri,intended_uri)
+ end
+
+end
+
